37f405a24c5357d5c7dc4c3d2791f9fa1365cca9
Gembali Lokesh <gembali.lokesh2004@example.com>
2025-07-06 23:10:08 +0530

    controller changes

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,14 +3,16 @@ const router = express.Router();
 const {
   getAllDoctors,
   getAllPatients,
-  getAllAppointments,registerAdmin,loginAdmin
-} = require("../controllers/adminController");
+  getAllAppointments,
+  registerAdmin,
+  loginAdmin,
+} = require("../controllers/admincontroller");
 
 const validateToken = require("../middleware/validateTokenHandler"); // you must create this
 
-router.get("/doctors", validateToken,getAllDoctors);
-router.get("/patients",validateToken, getAllPatients);
-router.get("/appointments",validateToken, getAllAppointments);
-router.post('/register',registerAdmin);
-router.post('/login',loginAdmin);
+router.get("/doctors", validateToken, getAllDoctors);
+router.get("/patients", validateToken, getAllPatients);
+router.get("/appointments", validateToken, getAllAppointments);
+router.post("/register", registerAdmin);
+router.post("/login", loginAdmin);
 module.exports = router;
